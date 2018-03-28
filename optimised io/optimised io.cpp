8b16c34e1e90a1bbafc56a5e1571fd6084a9c894
Hzc492 <Hzc492@users.noreply.github.com>
2018-03-28 12:56:20 +0800

    读写优化

---
+++ optimised io/optimised io.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+using LL=long long;
+#define FOR(i,x,y) for(decay<decltype(y)>::type i=(x),_##i=(y);i<=_##i;++i)
+#define FORD(i,x,y) for(decay<decltype(x)>::type i=(x),_##i=(y);i>=_##i;--i)
+#define mem(f,x) memset(f,x,sizeof(f))
+//=======================================================
+void hin(long &res){
+    char ch;
+    bool flag=false;
+    while (!isdigit(ch=getchar()))
+        if (ch=='-') flag=true;
+    for (res=ch-'0';isdigit(ch=getchar());res=res*10+ch-'0');
+    if (flag) res=-res;
+}
+void hout(long x){
+    if (x<0){
+        putchar('-');
+        x=-x;
+    }
+    if (x>9) hout(x/10);
+    putchar(x%10+'0');
+}
+int main(){
+    long n;
+    hin(n);
+    hout(n);
+}
