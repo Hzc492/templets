0a059208726d80c0b26db21b4dac1d855cc4d827
Hzc492 <Hzc492@users.noreply.github.com>
2018-08-21 13:36:46 +0800

    树链剖分

--- 树链剖分/树链剖分.cpp
+++ 树链剖分/树链剖分.cpp
@@ -1,10 +1,4 @@
-#include <bits/stdc++.h>
-using namespace std;
-using LL = long long;
-#define FOR(i, x, y) for (decay<decltype(y)>::type i = (x), _##i = (y); i <= _##i; ++i)
-#define FORD(i, x, y) for (decay<decltype(x)>::type i = (x), _##i = (y); i >= _##i; --i)
-#define mem(f,x) memset(f,x,sizeof(f))
-//========================================================================
+/*树链剖分 2次dfs+线段树维护*/
 const int MAX = 100000;
 int n, m, rot, MOD;
 int cnt;
