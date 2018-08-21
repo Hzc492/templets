544557084b34f78f6acd9ee5a60ebf58fd92c100
Hzc492 <Hzc492@users.noreply.github.com>
2018-08-21 13:30:26 +0800

    kmp

--- KMP/KMP.cpp
+++ KMP/KMP.cpp
@@ -1,10 +1,4 @@
-#include <bits/stdc++.h>
-using namespace std;
-using LL = long long;
-#define FOR(i, x, y) for (decay<decltype(y)>::type i = (x), _##i = (y); i <= _##i; ++i)
-#define FORD(i, x, y) for (decay<decltype(x)>::type i = (x), _##i = (y); i >= _##i; --i)
-#define mem(f,x) memset(f,x,sizeof(f))
-//========================================================================
+/*KMP字符串匹配*/
 string s1, s2;
 int f[1000005];
 int main()
