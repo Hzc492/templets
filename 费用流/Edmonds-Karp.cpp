a51a9bc4f7d2cc22c1c63501c8c679f47dcf9c45
Hzc492 <Hzc492@users.noreply.github.com>
2018-08-21 13:35:24 +0800

    最小费用最大流

--- 费用流/Edmonds-Karp.cpp
+++ 费用流/Edmonds-Karp.cpp
@@ -1,10 +1,4 @@
-#include <bits/stdc++.h>
-using namespace std;
-using LL = long long;
-#define FOR(i, x, y) for (decay<decltype(y)>::type i = (x), _##i = (y); i <= _##i; ++i)
-#define FORD(i, x, y) for (decay<decltype(x)>::type i = (x), _##i = (y); i >= _##i; --i)
-#define mem(f,x) memset(f,x,sizeof(f))
-//========================================================================
+/*Edmonds-Karp 最小费用最大流 基于SPFA*/
 const int inf = 0x7fffffff;
 const int MAX = 5005;
 struct u {
